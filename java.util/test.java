a862252e367e93a80781f401cb391cda09fb51dc
yangcheng <717449318@qq.com>
2017-03-27 10:31:13 +0800

    添加web使用示例

--- java.util/test.java
+++ java.util/test.java
@@ -19,14 +19,14 @@ public class test {
 	public static void main(String[] args) {
 		// 短信平台访问-发送短信
 		Map<String, String> maps1 = new HashMap<String, String>();
-		maps1.put("phoneNum", "157********");// 短信平台用户名
-		maps1.put("tokenid", "******");// 短信平台用户名
+		maps1.put("phoneNum", "157********");// 手机号
+		maps1.put("tokenid", "******");// 唯一标识
 		String flag1 = NetWork.post("http://127.0.0.1:8080/PhoneCode/phonecode/base!sendCode.action", maps1);
 		System.out.println("反馈信息==" + flag1);
 		// 短信平台验证-验证验证码
 		Map<String, String> maps2 = new HashMap<String, String>();
-		maps2.put("phoneNum", "157********");// 短信平台用户名
-		maps2.put("code", "660163");// 短信平台用户名
+		maps2.put("phoneNum", "157********");// 手机号
+		maps2.put("code", "660163");// 验证码
 		String flag2 = NetWork.post("http://127.0.0.1:8080/PhoneCode/phonecode/base!valida.action", maps2);
 		System.out.println("反馈信息==" + flag2);
 
